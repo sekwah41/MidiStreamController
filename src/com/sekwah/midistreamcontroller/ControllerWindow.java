d57f0d5edc4a3160705391fedc4c93731ef17c52
Alastair <alastair.hawkes@gmail.com>
2017-07-22 15:57:12 +0100

    Initial commit

---
+++ src/com/sekwah/midistreamcontroller/ControllerWindow.java
@@ -0,0 +1,18 @@
+package com.sekwah.midistreamcontroller;
+
+import javax.swing.*;
+
+public class ControllerWindow extends JFrame {
+
+    public ControllerWindow() {
+
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        this.setTitle("SekC's Stream Controller");
+
+        super.setVisible(true);
+    }
+
+
+
+}
---
+++ src/com/sekwah/midistreamcontroller/StreamController.java
@@ -0,0 +1,13 @@
+package com.sekwah.midistreamcontroller;
+
+/**
+ * This is designed for the launchpad however this could be recoded to any.
+ */
+public class StreamController {
+
+
+    public static void main(String[] args) {
+        ControllerWindow mainWindow = new ControllerWindow();
+    }
+
+}
